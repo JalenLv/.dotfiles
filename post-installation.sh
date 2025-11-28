dff9cde1ef3c6733c13af8666772789d98e839d5
JalenLv <jalenlv543@qq.com>
2025-11-28 10:56:21 -0600

    Add -y flags for unattended apt installation.

--- post-installation.sh
+++ post-installation.sh
@@ -186,7 +186,7 @@ read -r -p "Do you want to install Docker? [Y/n] "
 if [[ $REPLY =~ ^[Yy]$ ]] || [[ -z $REPLY ]]; then
     echo "Setting up Docker repository..."
     # Add Docker's official GPG key:
-    sudo apt install ca-certificates
+    sudo apt install -y ca-certificates
     sudo install -m 0755 -d /etc/apt/keyrings
     sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
     sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -202,7 +202,7 @@ EOF
     sudo apt update
 
     echo "Installing Docker..."
-    sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
     echo "Adding user $USER to docker group..."
     sudo groupadd docker || true
