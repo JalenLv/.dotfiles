953c78df25ae76de37687c5b6c1af3af2e9f4ecb
JalenLv <jalenlv543@qq.com>
2025-11-28 10:54:37 -0600

    Add Docker installation

--- post-installation.sh
+++ post-installation.sh
@@ -181,6 +181,37 @@ else
     echo "Skipping Visual Studio Code installation."
 fi
 
+# Docker
+read -r -p "Do you want to install Docker? [Y/n] "
+if [[ $REPLY =~ ^[Yy]$ ]] || [[ -z $REPLY ]]; then
+    echo "Setting up Docker repository..."
+    # Add Docker's official GPG key:
+    sudo apt install ca-certificates
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+    # Add the repository to Apt sources:
+    sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+    sudo apt update
+
+    echo "Installing Docker..."
+    sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+    echo "Adding user $USER to docker group..."
+    sudo groupadd docker || true
+    sudo usermod -aG docker $USER
+    newgrp docker
+else
+    echo "Skipping Docker installation."
+fi
+
 # NoMachine
 read -r -p "Do you want to install NoMachine? [Y/n] "
 if [[ $REPLY =~ ^[Yy]$ ]] || [[ -z $REPLY ]]; then
